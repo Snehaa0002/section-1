0ed96fe2769a0717658e62e3a55c07a6c34d71ad
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-04-03 17:32:12 +0530

    map and filter

--- section 1/map_filter.js
+++ section 1/map_filter.js
@@ -1,21 +1,21 @@
-const nums = [4, 8,3, 1,6,9];
+const nums = [4 , 8 , 3 , 1 , 6 , 9];
 
-const nweNums =[];
+const newNums =[];
 for(let n of nums){
-    // console.log(n  ** 3);node
-    nweNums.push(n ** 3);
-} 
+    // console.log(n ** 3);
+    newNums.push(n ** 3);
+}
+
 console.log(newNums);
 
-const  newArr = nums.map((a) => {return a**3} );
+const newArr = nums.map( (a) => { return a*2 } );
 console.log(newArr);
 
-const names = ['ravi', 'sneha','mahi', 'muskan','parul'];
+const names =["Sneha" , "Ravi" , "Parul" , "Bhavi" , "Muskan"];
 
-console.log('abc', toUpperCase() );
-const upperCaseNames = names.map( (n) => {return n.toUpperCase() } );
-console.log(upperCaseNames);
+// console.log( 'abc'.toUpperCase() );
+ 
+console.log( 'abc'.toUpperCase() );
+const upperCaseName = names.map( (n) => { return n. toUpperCase() } );
 
-
-
-
+console.log(upperCaseName);
