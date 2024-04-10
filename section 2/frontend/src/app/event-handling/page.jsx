ef26aabb8962b621c7c104a07129a08d736f162e
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-04-10 14:27:30 +0530

    event handling

--- section 2/frontend/src/app/event-handling/page.jsx
+++ section 2/frontend/src/app/event-handling/page.jsx
@@ -1,16 +1,15 @@
-'use client';
+'use client'
 import React from 'react'
 
 const EventHandling = () => {
-
   return (
     <div>
         <h1>Event Handling</h1>
         <hr />
-        <button className="btn btn-primary"  type="button" class="btn btn-outline-primary" onClick={() => { alert('')}}>Click Me</button>
-            </div>
 
+        <button className='btn btn-primary' onClick={ () =>  { alert('Huraayyyyy....!') } }>Click Me!</button>
+    </div>
   )
 }
 
-export default Eventhandling;
+export default EventHandling;
