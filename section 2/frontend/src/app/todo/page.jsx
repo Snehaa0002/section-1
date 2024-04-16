c7d0ad9a207b68bd86352bf0fd4b67aa79ae8847
Snehaa0002 <163520243+Snehaa0002@users.noreply.github.com>
2024-04-16 15:33:20 +0530

    signup

--- section 2/frontend/src/app/todo/page.jsx
+++ section 2/frontend/src/app/todo/page.jsx
@@ -1,35 +1,30 @@
-'use client';
-import React, { useState }from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Todo = () => {
 
-    const [todoList,setTodoList] = useState([
-        { task : 'Do homework', completed: false },
-        { task : 'Eat Food', completed: false },
-        { task : 'Play Games', completed: false },
-        { task : 'Play Games', completed: false },
-        
+    const [todoList, setTodoList] = useState([
+    //    { task : 'Do homework', completed: false },
+    //    { task : 'Eat Food', completed: false },
+    //    { task : 'Play Games', completed: false },
+    //    { task : 'Sleep', completed: false }
 
     ]);
 
-    const [a, setA] = useState(10);
-
-    const addTask = (e) => { 
-
-        
+    const addTask = (e) => {
         if(e.code === 'Enter'){
-               console.log(e.target.value);
+            console.log(e.target.value);
 
-               setTodoList( [ ... todoList, { task : e.target.value, completed: false } ] );
-               e.target.value = '';
-         }
+            setTodoList( [ ...todoList, { task : e.target.value, completed : false}] );
+            e.target.value = '';
 
-        
+        }
+
     }
 
     const deleteTask = (index) => {
         console.log(index);
-        const temp = todoList;
+        const temp  = todoList;
         temp.splice(index, 1);
         setTodoList([...temp]);
     }
@@ -37,10 +32,10 @@ const Todo = () => {
     const finishTask = (index) => {
         const temp = todoList;
         temp[index].completed = !temp[index].completed;
-        console.log(temp );
+        console.log(temp);
         setTodoList([...temp]);
     }
-    
+
   return (
     <div className='container py-5'>
 
@@ -50,35 +45,32 @@ const Todo = () => {
         <div className='card'>
             <div className='card-header'>
                 <input onKeyDown={addTask} type="text" className='form-control border-primary border-2' />
-               </div>
-               < div className="card-body" >
+            </div>
+            <div className='card-body'>
                 {
-                     todoList.map( (item,index) => {
-                        return <div key={index} className='d-flex justify-content-between p-3'>
+                    todoList.map( (item, index) => {
+                        return <div className='d-flex justify-content-between p-3'>
                             <p>{item.task}</p>
 
-                            { item.completed ?
-                            <span className='badge bg-sucess'>pending</span>
-                            :
-                            <span className='badge bg-warning'>pending</span>
-                     }
-
-
-
+                            { item.completed ? 
+                                <span className='badge bg-success'>Completed</span>
+                                :
+                                <span className='badge bg-warning'>Pending</span>                     
+                        }
                             <div>
-                              <button onclick={() => { finishTask( index) }} className='btn btn-primary me-3'>
-                                {
-                                   item.completed ? 'undo' : 
-                            
-                              <button onclick={() => { deleteTask( index) }} className='btn btn-danger'>Delete</button>
+                                <button onClick={ () => { finishTask(index) } } className='btn btn-primary me-3'>
+                                    {
+                                        item.completed ? 'undo' : 'Finish'
+                                    }
+                                </button>
+                                <button onClick={ () => { deleteTask(index) } } className='btn btn-danger'>Delete</button>
                             </div>
-                            </div>
-                     } )
+                        </div>
+                    } )
                 }
+            </div>
         </div>
     </div>
-    </div>
-
   )
 }
 
